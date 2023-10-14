d82908a056572464563a13dfc80a00df89f1ec89
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-10-14 21:44:42 +1100

    added facebook comments

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-center">
+  <div class="flex flex-col items-center justify-center">
     <div class="flex w-[900px] max-w-[900px] min-w-0 items-center mt-8">
       <div class="flex flex-col mr-8">
         <p class="font-bold text-4xl mb-6">
@@ -12,6 +12,9 @@
         </p>
       </div>
       <img src="../assets/it-support.jpg" class="w-[550px]" />
+    </div>
+    <div class="flex">
+      <iframe class="mt-16 mr-4" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fjenny.c.barber%2Fposts%2Fpfbid0kbfms467JhxEQox25LwPLHworStTFJGPbTz1KbKqbUwdM6H4njQJSGuJLEa3s5JBl&show_text=true&width=500" width="500" height="169" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
     </div>
   </div>
 </template>
