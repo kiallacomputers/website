90db552a7ed1899497892e23900ab7f153e741ee
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-10-15 17:43:46 +1100

    tweaked icons page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -10,21 +10,21 @@
         <br>
         
         <div class="ml-20 mt-10 flex items-center justify-center">
-          <img class="w-8 h-8 mr-4" src="../assets/envelope.svg">
+          <img class="w-8 h-8" src="../assets/envelope.svg">
         </div>
         <div class="mb-10 ml-20 flex items-center justify-center">
           <a class="font-bold" href="mailto:sales@kiallacomputers.com.au">sales@kiallacomputers.com.au</a>
         </div>
 
         <div class=" ml-20 flex items-center justify-center">
-          <img class="w-8 h-8 mr-4" src="../assets/phone.svg">
+          <img class="w-8 h-8" src="../assets/phone.svg">
         </div>
         <div class="mb-10 ml-20 flex items-center justify-center">
            <b><a href="tel:+61457826441">0457-826-441</a></b>
         </div>
 
         <div class="ml-20 flex items-center justify-center">
-          <img class="w-8 h-8 mr-4" src="../assets/facebook.svg">
+          <img class="w-8 h-8" src="../assets/facebook.svg">
         </div>
         <div class="mb-10 ml-20 flex items-center justify-center">
           <a class="font-bold" href="https://www.facebook.com/kiallacomputers" target="_blank">kiallacomputers</a>
