2bfa6343acaf2c95b08b82447264de3bb2dd78f4
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-10-15 11:40:50 +1100

    images to conact page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,31 +1,29 @@
 <template>
   <div class="flex items-center justify-center">
     <div class="flex items-center w-[900px] mt-2">
-      <div class="flex flex-col mt-4">
-        <p class="font-bold text-4xl mb-6 text-center">
+      <div class="flex flex-col mt-10 w-full">
+        
+        <p class="font-bold text-4xl mb-6 text-center w-full">
           Love to hear from you, please reach out
         </p>
-        <p class="mb-4">
-          Email:
-          <a class="font-bold" href="mailto:sales@kiallacomputers.com.au"
-            >sales@kiallacomputers.com.au</a
-          >
-        </p>
+        
+        <br>
+        
+        <div class="mb-4 ml-20 mt-10 flex items-center">
+          <img class="w-8 h-8 mr-4" src="../assets/envelope.svg">
+          <a class="font-bold" href="mailto:sales@kiallacomputers.com.au">sales@kiallacomputers.com.au</a>
+        </div>
 
-        <p class="mb-4">
-          Phone:
+        <div class="mb-4 ml-20 flex items-center">
+          <img class="w-8 h-8 mr-4" src="../assets/phone.svg">
            <b>0457-826-441</b>
-        </p>
+        </div>
 
-        <p>
-          Facebook:
-          <a
-            class="font-bold"
-            href="https://www.facebook.com/kiallacomputers"
-            target="_blank"
-            >kiallacomputers</a
-          >
-        </p>
+        <div class="mb-4 ml-20 flex items-center">
+          <img class="w-8 h-8 mr-4" src="../assets/facebook.svg">
+          <a class="font-bold" href="https://www.facebook.com/kiallacomputers" target="_blank">kiallacomputers</a>
+        </div>
+
       </div>
     </div>
   </div>
