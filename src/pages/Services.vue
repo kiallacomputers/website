374c12378fe5d7adf96794f5efd602eb999f7e7b
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-10-15 14:28:41 +1100

    updating service page

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -1,33 +1,121 @@
 <template>
+  
   <div class="flex items-center justify-center">
+    
     <div class="flex items-center w-[900px] mt-2">
+      
       <div class="flex flex-col mt-4 w-full">
-        <p class="font-bold text-2xl mb-6">
-          We offer a wide range of services for home and small businesses
-          including
-        </p>
 
-        <div class="mb-4 ml-20 mt-10 flex items-center">
-          <h2>Computer and laptop repairs</h2>
-          <br>
-          <img class="w-8 h-8 mr-4" src="../assets/computer.svg">
+        <p class="font-bold text-5xl text-2xl mb-6 text-center w-full">Services We Offer</p>
+        
+        <div class="mb-10 mt-10 w-full">
+
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/computer.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">Computer and Latop Repairs</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/security.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">Virus protection installation</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/malware.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">Malware removal</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/wifi.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">Ethernet and Wifi Setup</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/printer.svg"><img class="w-20 h-20" src="../assets/scanner.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">Printer and Scanner Setup</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/ipad.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">iPad and Tablet support</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/smarttv.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">TV and SmartTV setup</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/data.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">Data backup and recovery</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/modem.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">NBN and broadband setup</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/homeoffice.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">Home office setup and support</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/emailsetup.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">Email intergration for business</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center">
+            <img class="w-20 h-20" src="../assets/security2.svg">
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">Business security setup</a>
+          </div>
+          
+          <div class="flex items-center w-full  justify-center mb-10">
+            <a class="text-xl font-extrabold">And much more... Just contact us!</a>
+          </div>
+        
         </div>
-
-        <ul class="list-disc ml-8">
-          <li>Virus protection installation</li>
-          <li>Malware removal</li>
-          <li>Ethernet and wifi setup</li>
-          <li>Printer and scanner setup</li>
-          <li>iPad and Tablet support</li>
-          <li>TV and SmartTV setup</li>
-          <li>Data backup and recovery</li>
-          <li>NBN and broadband setup</li>
-          <li>Home office setup and support</li>
-          <li>Email intergration for business</li>
-          <li>Business security setup</li>
-          <li>And much more... Just contact us!</li>
-        </ul>
+      
       </div>
+    
     </div>
+  
   </div>
+
 </template>
