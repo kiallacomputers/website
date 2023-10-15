66d800a8a7a91cc855f63e24d5289501aa129efe
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-10-15 14:53:14 +1100

    tweaked fornt page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -9,18 +9,24 @@
         
         <br>
         
-        <div class="mb-4 ml-20 mt-10 flex items-center">
+        <div class="ml-20 mt-10 flex items-center justify-center">
           <img class="w-8 h-8 mr-4" src="../assets/envelope.svg">
+        </div>
+        <div class="mb-10 ml-20 flex items-center justify-center">
           <a class="font-bold" href="mailto:sales@kiallacomputers.com.au">sales@kiallacomputers.com.au</a>
         </div>
 
-        <div class="mb-4 ml-20 flex items-center">
+        <div class=" ml-20 flex items-center justify-center">
           <img class="w-8 h-8 mr-4" src="../assets/phone.svg">
+        </div>
+        <div class="mb-10 ml-20 flex items-center justify-center">
            <b>0457-826-441</b>
         </div>
 
-        <div class="mb-4 ml-20 flex items-center">
+        <div class="ml-20 flex items-center justify-center">
           <img class="w-8 h-8 mr-4" src="../assets/facebook.svg">
+        </div>
+        <div class="mb-10 ml-20 flex items-center justify-center">
           <a class="font-bold" href="https://www.facebook.com/kiallacomputers" target="_blank">kiallacomputers</a>
         </div>
 
