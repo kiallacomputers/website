7be200402431e7e8a9ba840af914519cff661f2d
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-10-19 20:29:15 +1100

    updating Specials Page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import './lightbox.css'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue';
