953395896c89782d8ae58ac129f03a9434a7f286
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-10-19 14:53:37 +1100

    playing

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -17,10 +17,10 @@
           </div>
 
           <div class="flex flex-row items-center justify-center">
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l font-bold">Computer and Latop Repairs</a>
             </div>
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l ml-4 font-bold">Virus Protection Installation</a>
             </div>
           </div>
@@ -35,10 +35,10 @@
           </div>
 
           <div class="flex flex-row items-center justify-center">
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l ml-4 font-bold">Malware Removal</a>
             </div>
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l ml-4 font-bold">Ethernet and Wifi Setup</a>
             </div>
           </div>
@@ -56,10 +56,10 @@
           </div>
 
           <div class="flex flex-row items-center justify-center">
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l ml-4 font-bold">Printer and Scanner Setup</a>
             </div>
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l ml-4 font-bold">iPad and Tablet Support</a>
             </div>
           </div>
@@ -74,10 +74,10 @@
           </div>
 
           <div class="flex flex-row items-center justify-center">
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l ml-4 font-bold">TV and SmartTV Setup</a>
             </div>
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l ml-4 font-bold">Data Backup and Recovery</a>
             </div>
           </div>
@@ -92,10 +92,10 @@
           </div>
 
           <div class="flex flex-row items-center justify-center">
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l ml-4 font-bold">NBN and Broadband Setup</a>
             </div>
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l ml-4 font-bold">Home Office Setup and Support</a>
             </div>
           </div>
@@ -110,20 +110,20 @@
           </div>
 
           <div class="flex flex-row items-center justify-center">
-            <div class="flex items-center justify-center basis-1/2">
+            <div class="flex items-center justify-center basis-1/2 text-center w-full">
               <a class="text-l ml-4 font-bold"
                 >Email Intergration for Business</a
               >
             </div>
             <div
-              class="flex items-center justify-center basis-1/2 items-center justify-center"
+              class="flex items-center justify-center basis-1/2 text-center w-full"
             >
               <a class="text-l ml-4 font-bold">Business Security Setup</a>
             </div>
           </div>
 
           <div class="flex flex-row items-center justify-center mt-20">
-            <div class="flex basis-1/2 items-center justify-center">
+            <div class="flex basis-1/2 items-center justify-center text-center w-full">
               <a class="text-l font-bold"
                 >And much more... Just&nbsp;<a
                   class="text-l font-bold"
