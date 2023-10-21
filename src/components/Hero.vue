7a56d3cb37804eefbd2705bc342d1eba74fc2eec
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-10-21 19:43:46 +1100

    Updated Front Page

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-      <div class="flex flex-col mx-8 lg:mr-8 lg:ml-0">
+      <div class="flex flex-col mx-8 lg:mr-8 lg:ml-0 items-center justify-center">
         <p class="font-bold text-4xl">
           I.T. SUPPORT
         </p>
