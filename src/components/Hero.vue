f22495fc91eb6e3ef3e6991c3fa06417d442456d
Lachlan Hogan <imlocie@gmail.com>
2023-10-31 10:58:11 +1100

    Made the book a tech button animate on hover

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,15 @@
 <template>
-
   <div class="flex flex-col mx-8 lg:mr-8 lg:ml-8 items-center justify-center">
-    <div class="bg-[url('assets/hsbbgbanner.png')] w-[900px] text-center justify-center">
+    <div
+      class="bg-[url('assets/hsbbgbanner.png')] w-[900px] text-center justify-center"
+    >
       <p class="font-bold text-4xl font-quest text-[#144C84] mt-8 capitalize">
         I.T. SUPPORT
       </p>
-      <p class="font-extrabold text-2xl mb-6 font-quest text-[#144C84] capitalize">
-        for home and small businesses 
+      <p
+        class="font-extrabold text-2xl mb-6 font-quest text-[#144C84] capitalize"
+      >
+        for home and small businesses
       </p>
     </div>
   </div>
@@ -14,38 +17,68 @@
     <div class="flex w-[900px] max-w-full min-w-0 items-center mt-8">
       <div class="flex flex-col mx-8 lg:mr-8 lg:ml-0">
         <p class="text-lg mb-6 font-quest">
-          Kialla Computers is a cutting-edge computer company dedicated to providing a wide range of technology solutions
-          to meet the ever-evolving needs of individuals and businesses. With a passion for innovation and a commitment to 
-          excellence, we are your trusted partner in the digital age. 
+          Kialla Computers is a cutting-edge computer company dedicated to
+          providing a wide range of technology solutions to meet the
+          ever-evolving needs of individuals and businesses. With a passion for
+          innovation and a commitment to excellence, we are your trusted partner
+          in the digital age.
         </p>
         <p class="text-lg mb-6 font-quest">
-          At Kialla Computers, we believe that technology should enhance your life and business, not complicate it. We're 
-          here to simplify the world of computers and technology, ensuring that you're always connected, secure, and efficient. 
+          At Kialla Computers, we believe that technology should enhance your
+          life and business, not complicate it. We're here to simplify the world
+          of computers and technology, ensuring that you're always connected,
+          secure, and efficient.
         </p>
-        <p class="text-lg mb-6 font-quest">  
-          Contact us today to explore how Kialla Computers can empower your digital world.
+        <p class="text-lg mb-6 font-quest">
+          Contact us today to explore how Kialla Computers can empower your
+          digital world.
         </p>
       </div>
-      <img src="../assets/it-support.jpg" class="w-[450px] hidden lg:block mt-10" />
+      <img
+        src="../assets/it-support.jpg"
+        class="w-[450px] hidden lg:block mt-10"
+      />
     </div>
     <div class="flex felex-col mx-8 lg:mr-8 lg:ml-0">
-        <!-- Start Square Appointments Embed code --> 
-        <a target="_top" style=" background-color: #31C0DF; border: none; color: white; height: 40px; font-family: 'Questrial'; letter-spacing: 1px; line-height: 38px; padding: 0 28px; border-radius: 8px; font-weight: 500; font-size: 14px; cursor: pointer; display: inline-block; " href="https://squareup.com/appointments/book/skj9f2rh200xeg/LKXD0VFGAAZNS/start" rel="nofollow">Book a Tech</a> 
-        <!-- End Square Appointments Embed code -->
+      <!-- Start Square Appointments Embed code -->
+      <a
+        target="_top"
+        style="
+          background-color: #31c0df;
+          border: none;
+          color: white;
+          height: 40px;
+          font-family: 'Questrial';
+          letter-spacing: 1px;
+          line-height: 38px;
+          padding: 0 28px;
+          border-radius: 8px;
+          font-weight: 500;
+          font-size: 14px;
+          cursor: pointer;
+          display: inline-block;
+        "
+        href="https://squareup.com/appointments/book/skj9f2rh200xeg/LKXD0VFGAAZNS/start"
+        rel="nofollow"
+        class="hover:opacity-70 transition-all"
+        >Book a Tech</a
+      >
+      <!-- End Square Appointments Embed code -->
     </div>
 
     <!-- Start Facebook Reviews-->
     <div class="flex">
-      <iframe 
+      <iframe
         class="mt-16 mx-4 max-w-full"
-        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02SzEHwPumPStKnGpuy5nREoupND9hFcQQRew7dXSLQ9zFNi75C9vtdbGw747Z3TXul%26id%3D100008495259875&show_text=true&width=500" 
-        width="500" 
-        height="190" 
-        style="border:none;overflow:hidden;" 
-        scrolling="no" 
-        frameborder="0" 
-        allowfullscreen="true" 
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02SzEHwPumPStKnGpuy5nREoupND9hFcQQRew7dXSLQ9zFNi75C9vtdbGw747Z3TXul%26id%3D100008495259875&show_text=true&width=500"
+        width="500"
+        height="190"
+        style="border: none; overflow: hidden"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+      >
       </iframe>
     </div>
 
@@ -66,9 +99,15 @@
 
     <!-- Start Local Business Guide AD-->
     <div class="flex font-quest">
-      <div><a href="https://www.localbusinessguide.com.au/business/kialla-computers/"><img src="https://www.localbusinessguide.com.au/i/logo6.png"><br>Find us on Local Business Guide</a></div>
+      <div>
+        <a
+          href="https://www.localbusinessguide.com.au/business/kialla-computers/"
+          ><img
+            src="https://www.localbusinessguide.com.au/i/logo6.png"
+          /><br />Find us on Local Business Guide</a
+        >
+      </div>
     </div>
     <!-- End Local Business Guide AD-->
-
   </div>
 </template>
