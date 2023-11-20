b25fe18a0dbf28549367a38ce5edd8b8af1d59ca
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-11-21 10:32:44 +1100

    Updating Front Page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -10,9 +10,9 @@
 
         <br />
 
-        <div class="mt-10 flex items-center justify-center">
+        <div class="mt-2 flex items-center justify-center">
           <a class="font-bold" href="mailto:sales@kiallacomputers.com.au"
-            ><img class="w-8 h-8" src="../assets/envelope.svg" /></a
+            ><img class="w-12 h-12" src="../assets/envelope.svg" /></a
           >
         </div>
         <div class="mb-10 flex items-center justify-center font-quest">
@@ -33,7 +33,7 @@
             class="font-bold"
             href="https://www.facebook.com/kiallacomputers"
             target="_blank"
-            ><img class="w-8 h-8" src="../assets/facebook.svg" /></a
+            ><img class="w-12 h-12" src="../assets/facebook.svg" /></a
           >
         </div>
         <div class="mb-10 flex items-center justify-center font-quest">
@@ -43,6 +43,9 @@
             target="_blank"
             >kiallacomputers</a
           >
+        </div>
+        <div class="mb-1 flex items-center justify-center font-quest font-bold">
+          Where are we...
         </div>
         <div class="mb-10 flex items-center justify-center font-quest">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28193835.33683063!2d114.71197749879741!3d-30.3898545267493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad881fc25a9375b%3A0x515e7648819c14de!2sKialla%20Computers!5e0!3m2!1sen!2sau!4v1700382845842!5m2!1sen!2sau" width="300" :height="'300'" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
