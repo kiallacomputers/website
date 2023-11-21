b4002d4b2b89ecab465817cb1c37a932205e2afd
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-11-22 10:25:57 +1100

    Fixing conact.vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -44,7 +44,7 @@
         </div>
 
         <div class="mb-10 flex items-center justify-center font-quest">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3862.1169686438807!2d145.40221225743113!3d-36.43165989221269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad881fc25a9375b%3A0x515e7648819c14de!2sKialla%20Computers!5e1!3m2!1sen!2sau!4v1700523496265!5m2!1sen!2sau" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3862.1169686438807!2d145.40221225743113!3d-36.43165989221269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad881fc25a9375b%3A0x515e7648819c14de!2sKialla%20Computers!5e1!3m2!1sen!2sau!4v1700523496265!5m2!1sen!2sau" width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
       </div>
