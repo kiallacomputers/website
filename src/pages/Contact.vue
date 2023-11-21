3b93d116d3fa8fed5e3bbe2056afe49125186dc8
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2023-11-22 10:24:45 +1100

    Fixing conact.vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,56 +1,56 @@
 <template>
+
   <div class="flex items-center justify-center">
     <div class="flex items-center w-[900px] mt-2 justify-center">
       <div class="flex flex-col mt-10 w-full">
-        <p
-          class="font-bold text-4xl mb-6 text-center w-full justify-center font-quest"
-        >
+        <p class="font-bold text-4xl mb-6 text-center w-full justify-center font-quest">
           Love to hear from you!
         </p>
 
-        <br />
-
         <div class="mt-2 flex items-center justify-center">
-          <a class="font-bold" href="mailto:sales@kiallacomputers.com.au"
-            ><img class="w-12 h-12" src="../assets/envelope.svg" /></a
-          >
+          <a class="font-bold" href="mailto:sales@kiallacomputers.com.au">
+            <img class="w-12 h-12" src="../assets/envelope.svg">
+          </a>
         </div>
+
         <div class="mb-10 flex items-center justify-center font-quest">
-          <a class="font-bold" href="mailto:sales@kiallacomputers.com.au"
-            >sales@kiallacomputers.com.au</a
-          >
+          <a class="font-bold" href="mailto:sales@kiallacomputers.com.au">
+            sales@kiallacomputers.com.au
+          </a>
         </div>
 
         <div class="flex items-center justify-center">
           <img class="w-12 h-12" src="../assets/phone.svg" />
         </div>
-        <div class="mb-10 flex items-center justify-center font-quest">
-          <b>0457-826-441</b>
+
+        <div class="mb-10 flex items-center justify-center font-quest font-bold">
+          0457-826-441
         </div>
 
         <div class="flex items-center justify-center">
-          <a
-            class="font-bold"
-            href="https://www.facebook.com/kiallacomputers"
-            target="_blank"
-            ><img class="w-12 h-12" src="../assets/facebook.svg" /></a
-          >
+          <a class="font-bold" href="https://www.facebook.com/kiallacomputers" target="_blank">
+            <img class="w-12 h-12" src="../assets/facebook.svg" />
+          </a>
         </div>
+
         <div class="mb-10 flex items-center justify-center font-quest">
-          <a
-            class="font-bold"
-            href="https://www.facebook.com/kiallacomputers"
-            target="_blank"
-            >kiallacomputers</a
-          >
+          <a class="font-bold" href="https://www.facebook.com/kiallacomputers" target="_blank">
+            kiallacomputers
+          </a>
         </div>
+
         <div class="mb-1 flex items-center justify-center font-quest font-bold">
           Where are we...
         </div>
+
         <div class="mb-10 flex items-center justify-center font-quest">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3862.1169686438807!2d145.40221225743113!3d-36.43165989221269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad881fc25a9375b%3A0x515e7648819c14de!2sKialla%20Computers!5e1!3m2!1sen!2sau!4v1700523496265!5m2!1sen!2sau" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
+
       </div>
+
     </div>
+
   </div>
+
 </template>
