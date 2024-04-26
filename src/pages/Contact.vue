1853cc8e87c98cb3c91f96332ecc1832e0ea201c
kiallacomputers <143672068+kiallacomputers@users.noreply.github.com>
2024-04-26 21:01:20 +1000

    update Contact Page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,11 +1,15 @@
 <template>
+  <div class="flex flex-col mx-8 lg:mr-8 lg:ml-8 items-center justify-center">
+    <div class="bg-[url('assets/hsbbgbanner.png')] bg-no-repeat w-[900px] h-[156px] text-center justify-center">
+      <p class="font-bold text-4xl font-quest text-[#144C84] mt-10 capitalize">
+        LOVE TO HEAR FROM YOU!
+      </p>
+    </div>
+  </div>
 
   <div class="flex items-center justify-center">
     <div class="flex items-center w-[900px] mt-2 justify-center">
       <div class="flex flex-col mt-10 w-full">
-        <p class="font-bold text-4xl mb-6 text-center w-full justify-center font-quest">
-          Love to hear from you!
-        </p>
 
         <div class="mt-2 flex items-center justify-center">
           <a class="font-bold" href="mailto:sales@kiallacomputers.com.au">
