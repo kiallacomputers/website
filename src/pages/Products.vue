3b7885045c21651b929ad3b471ed37f45c6e48eb
ellascreations <maltys@live.com.au>
2025-03-14 23:10:45 +1100

    d

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -6,7 +6,7 @@ const products = ref([
     id: 0,
     prodid: "officepackage1",
     proddesc:
-      "Complete office PC package, AMD Ryzen 5500 CPU, MSI B450M Motherboard, 16GB Corsair DD4 Memory, 1TB Gen4 NVMe SSD Hard Drive, Cougar Purity Black Case, 650W power Supply, Shintaro Mouse & Keyboard, AOC 24inch 100Hz Monitor, Windows 11 Pro.",
+      "Complete office PC package<BR /> AMD Ryzen 5500 CPU, MSI B450M Motherboard, 16GB Corsair DD4 Memory, 1TB Gen4 NVMe SSD Hard Drive, Cougar Purity Black Case, 650W power Supply, Shintaro Mouse & Keyboard, AOC 24inch 100Hz Monitor, Windows 11 Pro.",
     prodprice: "$830.00ex",
     prodimage: "/products/productImage_officepackage1.jpg",
   },
