e4e489ae6d559d8ba6c6757bd74f0a9c484ec2d1
ellascreations <maltys@live.com.au>
2025-04-04 10:58:20 +1100

    update product page

--- src/stores/products.js
+++ src/stores/products.js
@@ -8,6 +8,16 @@ const products = [
     prodprice: "999",
     prodpercentoff: "5",
     prodimage: "/products/productImage_officepackage1.jpg",
-  },
+    },
+     {
+     id: 0,
+    prodid: "officepackage1",
+    prodtitle: "Basic Office Package",
+    proddesc:
+    "AMD Ryzen 5500 CPU, MSI B450M Motherboard, 16GB Corsair DD4 Memory, 1TB Gen4 NVMe SSD Hard Drive, Cougar Purity Black Case, 650W power Supply, Shintaro Mouse & Keyboard, AOC 24inch 100Hz Monitor, Windows 11 Pro.",
+    prodprice: "999",
+    prodpercentoff: "5",
+    prodimage: "/products/productImage_officepackage1.jpg",
+   },
 ];
 export default products;
