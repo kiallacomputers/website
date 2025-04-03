7f15c8906184e0009cd9dd8460a25bec6ef0f2fc
ellascreations <maltys@live.com.au>
2025-04-03 22:28:05 +1100

    updating Product Card

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,26 +4,30 @@
       <router-link to="/">
         <img
           src="../assets/kialla_logo_small.png"
-          class="h-14 md:h-20 cursor-pointer hover:opacity-60 transition-all" />
+          class="h-14 md:h-20 cursor-pointer hover:opacity-60 transition-all"
+        />
       </router-link>
       <div class="grow"></div>
       <router-link to="/services">
         <div
-          class="px-4 md:text-lg cursor-pointer hover:opacity-70 transition-all .inter">
+          class="px-4 md:text-lg cursor-pointer hover:opacity-70 transition-all .inter"
+        >
           Services
         </div>
       </router-link>
       <div class="grow"></div>
+      <!--
       <router-link to="/products">
         <div
           class="px-4 md:text-lg cursor-pointer hover:opacity-70 transition-all">
           Products
         </div>
       </router-link>
-      <div class="grow"></div>
+      <div class="grow"></div>-->
       <router-link to="/contact">
         <div
-          class="px-2 md:text-lg cursor-pointer hover:opacity-70 transition-all">
+          class="px-2 md:text-lg cursor-pointer hover:opacity-70 transition-all"
+        >
           Contact Us
         </div>
       </router-link>
--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import ProductCard from "../components/ProductCard.vue";
+
 import { ref } from "vue";
+
+function calculatePrecentage(percent, num) {
+  let discount = num * (percent / 100);
+  let newprice = num - discount;
+  return newprice.toFixed(0);
+}
 
 const products = ref([
   {
@@ -8,7 +16,18 @@ const products = ref([
     prodtitle: "Basic Office Package",
     proddesc:
       "AMD Ryzen 5500 CPU, MSI B450M Motherboard, 16GB Corsair DD4 Memory, 1TB Gen4 NVMe SSD Hard Drive, Cougar Purity Black Case, 650W power Supply, Shintaro Mouse & Keyboard, AOC 24inch 100Hz Monitor, Windows 11 Pro.",
-    prodprice: "$830.00ex",
+    prodprice: "999",
+    prodpercentoff: "",
+    prodimage: "/products/productImage_officepackage1.jpg",
+  },
+  {
+    id: 1,
+    prodid: "officepackage2",
+    prodtitle: "Basic Office Package 2",
+    proddesc:
+      "AMD Ryzen 5500 CPU, MSI B450M Motherboard, 16GB Corsair DD4 Memory, 1TB Gen4 NVMe SSD Hard Drive, Cougar Purity Black Case, 650W power Supply, Shintaro Mouse & Keyboard, AOC 24inch 100Hz Monitor, Windows 11 Pro.",
+    prodprice: "1100",
+    prodpercentoff: "15",
     prodimage: "/products/productImage_officepackage1.jpg",
   },
 ]);
@@ -24,87 +43,102 @@ const products = ref([
               <div class="flex flex-col mt-4 w-full">
                 <div class="mb-10 mt-10 w-full">
                   <div
-                    class="flex flex-row items-center justify-center py-3 text-start text-black uppercase"
-                  >
-                    <div class="flex basis-1/4 justify-center">Code</div>
-                    <div class="flex basis-1/5 justify-center">&nbsp;</div>
-                    <div class="flex basis-1/2">Description</div>
-                    <div class="flex basis-1/5 justify-center">Price</div>
-                  </div>
-                  <div
                     class="flex flex-row items-center justify-center w-fullpx-6 m-w-800px py-3 text-start text-black uppercase"
                     v-for="product in products"
                     :key="product.id"
                   >
-                    <div class="flex items-center basis-1/4 justify-center">
-                      {{ product.prodid }}
-                    </div>
-                    <div class="flex basis-1/5 items-center justify-center">
-                      <img :src="product.prodimage" width="100" />
-                    </div>
-                    <div class="flex basis-1/2">
-                      <div class="flex">
-                        <div>{{ product.prodtitle }}</div>
-                        <div>{{ product.proddesc }}</div>
+                    <!-- Product Card-->
+                    <div
+                      class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md"
+                    >
+                      <a
+                        class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
+                        href="#"
+                      >
+                        <img
+                          class="peer absolute top-0 right-0 h-full w-full object-cover"
+                          :src="product.prodimage"
+                        />
+                        <div
+                          class="absolute bottom-0 mb-4 flex space-x-4 w-full justify-center"
+                        >
+                          <div
+                            class="rounded-full h-3 w-3 bg-gray-200 border-2 border-white"
+                          ></div>
+                          <div
+                            class="rounded-full h-3 w-3 bg-gray-200 border-2 border-white"
+                          ></div>
+                          <div
+                            class="rounded-full h-3 w-3 bg-gray-200 border-2 border-white"
+                          ></div>
+                        </div>
+                        <span
+                          v-if="product.prodpercentoff"
+                          class="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white"
+                          >{{ product.prodpercentoff }}% OFF</span
+                        >
+                      </a>
+                      <div class="mt-4 px-5 pb-5">
+                        <a href="#">
+                          <h5 class="text-xl tracking-tight text-slate-900">
+                            {{ product.prodtitle }}
+                          </h5>
+                        </a>
+                        <div
+                          class="mt-2 mb-5 flex items-center justify-between"
+                        >
+                          <p>
+                            <span
+                              v-if="product.prodpercentoff"
+                              class="text-2xl font-bold text-slate-900 mr-1"
+                              >${{
+                                calculatePrecentage(
+                                  product.prodpercentoff,
+                                  product.prodprice
+                                )
+                              }}</span
+                            >
+                            <span
+                              v-if="!product.prodpercentoff"
+                              class="text-2xl font-bold text-slate-900 mr-1"
+                              >${{ product.prodprice }}</span
+                            >
+                            <span
+                              v-if="product.prodpercentoff"
+                              class="text-base font-bold text-red-900 line-through"
+                            >
+                              ${{ product.prodprice }}</span
+                            >
+                          </p>
+                        </div>
+                        <!--
+                        <a
+                          href="#"
+                          class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="mr-2 h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            stroke-width="2"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                            />
+                          </svg>
+                          Add to cart</a
+                        >-->
                       </div>
-                    </div>
-                    <div class="flex basis-1/5 justify-center">
-                      {{ product.prodprice }}
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <!--       
-          <table
-            class="max-w-900px divide-y divide-gray-200 dark:divide-neutral-700 md:table-fixed">
-            <thead>
-              <tr>
-                <td
-                  scope="col"
-                  class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
-                  Code
-                </td>
-                <td
-                  scope="col"
-                  class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"></td>
-                <td
-                  scope="col"
-                  class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 w-36">
-                  Description
-                </td>
-                <td
-                  scope="col"
-                  class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
-                  Price
-                </td>
-              </tr>
-            </thead>
-            <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
-              <tr
-                class="hover:bg-gray-100 dark:hover:bg-neutral-700"
-                v-for="product in products"
-                :key="product.id">
-                <td
-                  class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200 capitalize">
-                  {{ product.prodid }}
-                </td>
-                <td class="w-10 h-10">
-                  <img :src="product.prodimage" />
-                </td>
-                <td
-                  class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
-                  {{ product.proddesc }}
-                </td>
-                <td
-                  class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
-                  {{ product.prodprice }}
-                </td>
-              </tr>
-            </tbody>
-          </table>-->
         </div>
       </div>
     </div>
