5a44264a4cd0fc61841cf477bb15b2e7bdf5e9bf
ellascreations <maltys@live.com.au>
2025-04-04 11:02:28 +1100

    updating

--- src/stores/products.js
+++ src/stores/products.js
@@ -2,21 +2,21 @@ const products = [
   {
     id: 0,
     prodid: "officepackage1",
-    prodtitle: "Basic Office Package",
+    prodtitle: "Basic Office Package No. 1",
     proddesc:
       "AMD Ryzen 5500 CPU, MSI B450M Motherboard, 16GB Corsair DD4 Memory, 1TB Gen4 NVMe SSD Hard Drive, Cougar Purity Black Case, 650W power Supply, Shintaro Mouse & Keyboard, AOC 24inch 100Hz Monitor, Windows 11 Pro.",
     prodprice: "999",
-    prodpercentoff: "5",
+    prodpercentoff: "15",
     prodimage: "/products/productImage_officepackage1.jpg",
     },
      {
      id: 0,
-    prodid: "officepackage1",
-    prodtitle: "Basic Office Package",
+    prodid: "officepackage2",
+    prodtitle: "Basic Office Package No. 2",
     proddesc:
     "AMD Ryzen 5500 CPU, MSI B450M Motherboard, 16GB Corsair DD4 Memory, 1TB Gen4 NVMe SSD Hard Drive, Cougar Purity Black Case, 650W power Supply, Shintaro Mouse & Keyboard, AOC 24inch 100Hz Monitor, Windows 11 Pro.",
-    prodprice: "999",
-    prodpercentoff: "5",
+    prodprice: "1099",
+    prodpercentoff: "15",
     prodimage: "/products/productImage_officepackage1.jpg",
    },
 ];
