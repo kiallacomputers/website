731e1132fc991efc0bb9b835c7329966da14a31f
ellascreations <maltys@live.com.au>
2025-04-07 23:39:21 +1000

    .

--- src/stores/products.js
+++ src/stores/products.js
@@ -2,17 +2,17 @@ const products = [
   {
     id: 0,
     prodid: "officepackage1",
-    prodtitle: "Basic Office Package No. 1",
+    prodtitle: "Test Product",
     proddesc:
       "AMD Ryzen 5500 CPU, MSI B450M Motherboard, 16GB Corsair DD4 Memory, 1TB Gen4 NVMe SSD Hard Drive, Cougar Purity Black Case, 650W power Supply, Shintaro Mouse & Keyboard, AOC 24inch 100Hz Monitor, Windows 11 Pro.",
-    prodprice: "999",
+    prodprice: "0",
     prodpercentoff: "",
     prodimage: "/products/productImage_officepackage1.jpg",
     },
      {
      id: 1,
     prodid: "officepackage2",
-    prodtitle: "Basic Office Package No. 2",
+    prodtitle: "Test Product No. 2",
     proddesc:
     "AMD Ryzen 5500 CPU, MSI B450M Motherboard, 16GB Corsair DD4 Memory, 1TB Gen4 NVMe SSD Hard Drive, Cougar Purity Black Case, 650W power Supply, Shintaro Mouse & Keyboard, AOC 24inch 100Hz Monitor, Windows 11 Pro.",
     prodprice: "1099",
@@ -22,11 +22,11 @@ const products = [
      {
      id: 2,
     prodid: "officepackage3",
-    prodtitle: "Basic Office Package No. 2",
+    prodtitle: "Test Product No. 3",
     proddesc:
     "AMD Ryzen 5500 CPU, MSI B450M Motherboard, 16GB Corsair DD4 Memory, 1TB Gen4 NVMe SSD Hard Drive, Cougar Purity Black Case, 650W power Supply, Shintaro Mouse & Keyboard, AOC 24inch 100Hz Monitor, Windows 11 Pro.",
-    prodprice: "1299",
-    prodpercentoff: "",
+    prodprice: "10",
+    prodpercentoff: "100",
     prodimage: "/products/productImage_officepackage1.jpg",
    },
 ];
