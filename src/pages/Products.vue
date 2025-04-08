9a3c588caa04edfbab8ab8ac88b81dde8646666c
ellascreations <maltys@live.com.au>
2025-04-08 17:52:16 +1000

    update product page

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -75,6 +75,7 @@ function calculatePrecentage(percent, num) {
               >
             </p>
           </div>
+          <div class="mb-2">Lead time 2-3 days for building</div>
           <a
             href="/contact"
             class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
