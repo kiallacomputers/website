25bc2b9560a2dcabe0438b8335d94ed04bf9f7e1
ellascreations <maltys@live.com.au>
2025-04-09 12:07:42 +1000

    update products

--- src/stores/products.js
+++ src/stores/products.js
@@ -6,7 +6,7 @@ const products = [
     proddesc: ["AMD Ryzen 3 4100 CPU 4 Core", "B450M PRO-VDH MAX - MSI B450M PRO-VDH MAX AM4 Motherboard", "Intergrated onboard graphics", "16GB DDR4 Memory", "1TB Gen4 NVMe SSD Hard Drive", "Antec Black Case", "500W power Supply", "Shintaro Mouse & Keyboard", "AOC 27inch 100Hz Monitor", "Windows 11 Pro + Office Pro 2021"],
     prodprice: "1050.00",
     prodpercentoff: "10",
-    prodimage: "/products/productImage_officepackage1.jpg",
+    prodimage: "/products/office1.jpg",
     },
   {
     id: 1,
@@ -15,7 +15,7 @@ const products = [
     proddesc: ["Intel Core i5 12400F 12th Gen 6 Core CPU", "Asus or MSI H610M Motherboard with Wifi", "Asus or MSI RTX3050 6G OC Video card", "Gskill or Corsair 32GB DDR4 Memory", "Crucial or similar 1TB Gen4 NVMe SSD Hard Drive", "Cougar Archon 2 Mesh Airflow Gaming Case with 3x ARGB Fans  (White colour available - Please request as required)", "Cougar or MSI 600W ATC 80+ Bronze PSU", "Windows installation (if purchased with system)"],
     prodprice: "1325.00",
     prodpercentoff: "10",
-    prodimage: "/products/sysgi530501t32.jpg",
+    prodimage: "/products/guardiangamingpc32g.jpg",
     },
 ];
 export default products;
