6692efb08b1447a447e684e9c69d3fa51df1f142
ellascreations <maltys@live.com.au>
2025-09-10 20:13:00 +1000

    Updating Website

---
+++ pages/datarecovery.vue
@@ -0,0 +1,16 @@
+<script setup>import { useHead } from '#app';
+
+useHead({
+  title: 'Kialla Computers - Data Recovery',
+});
+
+definePageMeta({
+  layout: "landing",
+});
+</script>
+
+<template>
+  <LandingContainer>
+    <LandingDrc></LandingDrc>
+  </LandingContainer>
+</template>
