26ce5f3387b8080119185bd4cd2d3d8517daa0f4
Darren Waack <sales@kiallacomputers.com.au>
2026-03-22 17:26:27 +1100

    .

--- components/landing/solution.vue
+++ components/landing/solution.vue
@@ -3,7 +3,7 @@
     <div class="p-24 md:order-1 hidden md:block">
       <img
         class="rounded-full transition-transform transform hover:scale-125"
-        src="~/assets/img/dr.png"
+        src="~/assets/img/wifimesh.png"
         loading="eager"
         format="avif"
         width="512"
@@ -15,21 +15,35 @@
       <h1
         class="text-4xl lg:text-6xl xl:text-7xl text-center font-bold lg:tracking-tight text-MainHeader"
       >
-        Data Recovery.
+        Wifi Mesh
       </h1>
-      <h2 class="text-xl lg:text-2xl xl:text-4xl text-center font-semibold lg:tracking-tight text-MainHeader">$220/H</h2>
+      <h2
+        class="text-xl lg:text-2xl xl:text-4xl text-center font-semibold lg:tracking-tight text-MainHeader"
+      >
+        $220/H
+      </h2>
       <p class="text-lg mt-4 text-Primary max-w-xl">
-        We specialize in recovering lost, deleted, corrupted, or inaccessible data
-        from a wide range of storage devices. Whether the cause is accidental deletion, 
-        hardware failure, system crash, or malware infection, our advanced tools and 
-        techniques give you the best chance of restoring valuable information.
+        We specialize in recovering lost, deleted, corrupted, or inaccessible
+        data from a wide range of storage devices. Whether the cause is
+        accidental deletion, hardware failure, system crash, or malware
+        infection, our advanced tools and techniques give you the best chance of
+        restoring valuable information.
       </p>
       <ul class="grid mt-8 text-left gap-y-4">
-        <li class="flex items-start gap-3 text-ServiceItems"><LandingTick className="w-6 h-6" />Hard Drive & SSD Recovery</li>
-        <li class="flex items-start gap-3 text-ServiceItems"><LandingTick className="w-6 h-6" />USB & External Drive Recovery</li>
-        <li class="flex items-start gap-3 text-ServiceItems"><LandingTick className="w-6 h-6" />Laptop & Desktop Data Retrieval</li>
-        <li class="flex items-start gap-3 text-ServiceItems"><LandingTick className="w-6 h-6" />Malware or Ransomware Attacks Recovery</li>
-      </ul>  
+        <li class="flex items-start gap-3 text-ServiceItems">
+          <LandingTick className="w-6 h-6" />Hard Drive & SSD Recovery
+        </li>
+        <li class="flex items-start gap-3 text-ServiceItems">
+          <LandingTick className="w-6 h-6" />USB & External Drive Recovery
+        </li>
+        <li class="flex items-start gap-3 text-ServiceItems">
+          <LandingTick className="w-6 h-6" />Laptop & Desktop Data Retrieval
+        </li>
+        <li class="flex items-start gap-3 text-ServiceItems">
+          <LandingTick className="w-6 h-6" />Malware or Ransomware Attacks
+          Recovery
+        </li>
+      </ul>
     </div>
     <!-- <LandingSectionhead>
       <template v-slot:title>Data Recovery</template>
@@ -38,11 +52,8 @@
                             hardware failure, system crash, or malware infection, our advanced tools and 
                             techniques<br />give you the best chance of restoring valuable information.</template>
     </LandingSectionhead> -->
-</main>
-<div>
-<LandingDrContactForm />
-</div>
+  </main>
+  <div>
+    <LandingDrContactForm />
+  </div>
 </template>
-
-
-
