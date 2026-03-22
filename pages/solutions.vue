00285a59bb4d0fd110c8c81723821400ded83316
Darren Waack <sales@kiallacomputers.com.au>
2026-03-22 16:19:23 +1100

    .

---
+++ pages/solutions.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useHead } from "#app";
+
+useHead({
+  title: "Kialla Computers - Data Recovery",
+});
+
+definePageMeta({
+  layout: "landing",
+});
+</script>
+
+<template>
+  <LandingContainer>
+    <LandingDrc></LandingDrc>
+  </LandingContainer>
+</template>
