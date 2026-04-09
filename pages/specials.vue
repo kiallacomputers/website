6f5424a608fd3a2c1b243f5f9497c4327602fa24
Darren Waack <sales@kiallacomputers.com.au>
2026-04-09 18:06:11 +1000

    .

--- pages/specials.vue
+++ pages/specials.vue
@@ -9,6 +9,48 @@ useHead({
 });
 const specials = [
   {
+    name: "TP-Link Deco E4(2-pack) AC1200 Whole Home Mesh WiFi System",
+    price: "$185.00",
+    spec: "$160.00",
+    cost: "$130.64",
+    popular: true,
+    exp: "2026-04-30",
+    features: [
+      "TP-Link Mesh Technology Optional Ethernet backhaul work together to link Deco units to provide seamless coverage",
+      "2 Antennas (Internal) Multiple antennas form a signal-boosting array to cover more directions and large areas",
+      "Dual-Band Distribute devices to different bands for optimal performance",
+      "MU-MIMO Simultaneously communicates with multiple MU-MIMO clients",
+      "Router Mode & Access Point Mode",
+      "Dimensions mm (H x W x D) 90.7 x 90.1 x 190",
+    ],
+    image: "DECOE4-2P.jpg",
+    button: {
+      text: "Enquire",
+      link: "mailto:sales@kiallacomputers.com.au?Subject=Product Enquery TP Link DECOE4-2P",
+    },
+  },
+  {
+    name: "TP-Link Deco E4(3-pack) AC1200 Whole Home Mesh WiFi System",
+    price: "$230.00",
+    spec: "$215.00",
+    cost: "$168.04",
+    popular: true,
+    exp: "2026-04-30",
+    features: [
+      "TP-Link Mesh Technology Optional Ethernet backhaul work together to link Deco units to provide seamless coverage",
+      "2 Antennas (Internal) Multiple antennas form a signal-boosting array to cover more directions and large areas",
+      "Dual-Band Distribute devices to different bands for optimal performance",
+      "MU-MIMO Simultaneously communicates with multiple MU-MIMO clients",
+      "Router Mode & Access Point Mode",
+      "Dimensions mm (H x W x D) 90.7 x 90.1 x 190",
+    ],
+    image: "DECOE4-3P.jpg",
+    button: {
+      text: "Enquire",
+      link: "mailto:sales@kiallacomputers.com.au?Subject=Product Enquery TP Link DECOE4-3P",
+    },
+  },
+  {
     name: 'AOC C32G2ZE 31.5" Curve 240Hz 0.5ms Gaming Monitor',
     price: "$455.00",
     spec: "$410.00",
@@ -48,6 +90,27 @@ const specials = [
     button: {
       text: "Enquire",
       link: "mailto:sales@kiallacomputers.com.au?Subject=Product Enquery AOC C27G4Z",
+    },
+  },
+  {
+    name: 'AOC Q27G4SD 26.5" 2K 360Hz OLED Gaming Monitor',
+    price: "$1200.00",
+    spec: "$1050.00",
+    cost: "$808.50",
+    popular: true,
+    exp: "2026-04-25",
+    features: [
+      "360Hz Refresh Rate",
+      "<1ms Response Time",
+      "1000CR Curved",
+      "Total-Immersion Gaming",
+      "HDMI: 2.2, DisplayPort: 2.2",
+      "Dimensions mm (H x W x D) 516.4 x 611.52 x 239.4 inc Base",
+    ],
+    image: "AOCQ27G4SD.png",
+    button: {
+      text: "Enquire",
+      link: "mailto:sales@kiallacomputers.com.au?Subject=Product Enquery AOC Q27G4SD",
     },
   },
   {
